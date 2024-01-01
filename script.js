aac4c6a5b961e09201eaffacedbde976a27a347f
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2024-01-01 10:42:27 -0600

    Added functionality to copy password button

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const passwordBox = document.getElementById("password"); 
-const length = 12; 
+const length = 16; 
+const copyBtn = document.getElementById("copyBtn"); 
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbols = "@#$%^&*()_+|}{[]></-=";
 
-const allChars = upperCase + lowerCase + symbols + numbers +;
+const allChars = upperCase + lowerCase + symbols + number;
 
 function createPassword() {
     let password = ""; 
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
-    password += symbol[Math.floor(Math.random() * symbol.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
 
     while(length > password.length) {
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
     passwordBox.value = password;
+}
+
+
+function copyPassword() {
+    passwordBox.select(); 
+    document.execCommand("copy");
 }
 
 
